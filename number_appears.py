7eabbfa6b056e554af3dd2fc08ae21c30eb704ff
chanduranwala <chanduranwala@gmail.com>
2025-06-23 21:18:52 +0200

    Modify the code, 23.06

--- number_appears.py
+++ number_appears.py
@@ -1,20 +1,19 @@
+def string_to_numbers(input_value):
+    return [int(char.strip()) for char in input_value.split(",")]
+
+def counter(numbers, target):
+    # count = 0
+    # numbers = string_to_numbers(input_string)
+    # for number in numbers:
+    #     if number == seeking_number:
+    #         count = count + 1
+
+    count = numbers.count(target)
+    print(f"Then number {target} appears {count} in the list")
+
 input_string = input("Enter a list of numbers separated by a comma: ")
 seeking_number = int(input("Enter the number you want to count: "))
-
-def string_to_numbers(input_value):
-    char_list = input_value.split(",")
-    print(char_list)
-    return [int(char) for char in char_list]
-
-def counter():
-    count = 0
-    numbers = string_to_numbers(input_string)
-    for number in numbers:
-        if number == seeking_number:
-            count = count + 1
-
-    print(f"Then number {seeking_number} appears {count} in the list")
-
-counter()
+number_list = string_to_numbers(input_string)
+counter(number_list, seeking_number)
 
 
