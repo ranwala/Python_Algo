552ad520c0b9fa6380f044c653702158733140b0
chanduranwala <chanduranwala@gmail.com>
2025-09-09 20:25:52 +0200

    Modify date command line stopwatch, 09,09

--- daily_projects/command_line_stopwatch.py
+++ daily_projects/command_line_stopwatch.py
@@ -3,10 +3,12 @@ import pandas as pd
 import os
 
 FILE_PATH = '../files/daily_project/stopwatch_sessions.csv'
-datetime_start = None
+
+# Globals
+start_time = None
+pause_time = None
 lap_count = 0
-total_time_duration = timedelta(0)
-lap_duration_dict = {}
+laps = {}
 
 def store(data_dict):
     df = pd.DataFrame(data_dict)
@@ -28,57 +30,89 @@ def time_converter(time_difference: timedelta):
 
 
 while True:
-    command = input("Command (start/lap/pause/resume/stop/restart/quit): ")
+    command = input("Command (start/lap/pause/resume/stop/restart/quit): ").strip().lower()
 
-    if command.lower() == 'start':
-        datetime_start = datetime.now()
-        print(f'Started at {datetime_start.strftime('%H:%M:%S')}')
+    if command == 'start':
+        start_time = datetime.now()
+        print(f'Started at {start_time.strftime('%H:%M:%S')}')
 
-    elif command.lower() == 'lap':
+    elif command == 'lap':
+        if start_time is None:
+            print("Stopwatch has not start yet.")
+            continue
+
         lap_count += 1
         now = datetime.now()
-        time_duration = time_converter(now - datetime_start)
-        total_time_duration += time_duration
-        lap_duration_dict[f'Lap {lap_count}'] = time_duration
-        print(f'Lap {lap_count} - {time_duration} (Total: {total_time_duration})')
+        total_time_duration = now - start_time if pause_time is None else pause_time - start_time
+        lap_time_duration = total_time_duration if lap_count == 1 \
+            else total_time_duration - sum(laps.values(), timedelta())
 
-    elif command.lower() == 'pause':
-        print(f'Pause at {total_time_duration}')
+        laps[f'Lap {lap_count}'] = lap_time_duration
+        print(f'Lap {lap_count} - {time_converter(lap_time_duration)} (Total: {time_converter(total_time_duration)})')
 
-    elif command.lower() == 'resume':
-        print('Resumed.')
-        datetime_start = datetime.now()
+    elif command == 'pause':
+        if pause_time is None:
+            pause_time = datetime.now()
+            print(f'Pause at {time_converter(pause_time - start_time)}')
+        else:
+            print("Already paused")
 
-    elif command.lower() == 'stop':
-        fastest_key = min(lap_duration_dict, key=lap_duration_dict.get)
-        slowest_key = max(lap_duration_dict, key=lap_duration_dict.get)
-        average = total_time_duration / lap_count
+    elif command == 'resume':
+        if pause_time is not None:
+            pause_duration = datetime.now() - pause_time
+            start_time = start_time + pause_duration
+            pause_time = None
+            print('Resumed.')
+        else:
+            print("Stopwatch has not paused")
+
+    elif command == 'stop':
+        if start_time is None:
+            print("Stopwatch has not start yet.")
+            continue
+
+        end_time = pause_time if pause_time else datetime.now()
+        total_duration = end_time - start_time
+
+        fastest = min(laps, key=laps.get)
+        slowest = max(laps, key=laps.get)
+        average = sum(laps.values(), timedelta()) / len(laps)
 
         content = f"""
         Summery
         -------
         
-        Total Time: {total_time_duration}
+        Total Time: {time_converter(total_duration)}
         Laps: {lap_count}
-        Fastest Lap: {lap_duration_dict[fastest_key]} ({fastest_key})
-        Slowest Lap: {lap_duration_dict[slowest_key]} ({slowest_key})
-        Average Lap: {average}
+        Fastest Lap: {time_converter(laps[fastest])} ({fastest})
+        Slowest Lap: {time_converter(laps[slowest])} ({slowest})
+        Average Lap: {time_converter(average)}
         """
 
         print(content)
 
         data = {
-            'date': [datetime_start.strftime('%Y-%m-%d')],
-            'start_time': [datetime_start.strftime('%H:%M:%S')],
-            'total_duration': [str(total_time_duration)],
+            'date': [start_time.strftime('%Y-%m-%d')],
+            'start_time': [start_time.strftime('%H:%M:%S')],
+            'total_duration': [time_converter(total_duration)],
             'laps': [lap_count],
-            'fastest': [lap_duration_dict[fastest_key]],
-            'slowest': [lap_duration_dict[slowest_key]],
-            'average': [average]
+            'fastest': [time_converter(laps[fastest])],
+            'slowest': [time_converter(laps[slowest])],
+            'average': [time_converter(average)]
         }
 
         store(data)
+        break
 
+    elif command == "reset":
+        start_time = None
+        paused_time = None
+        lap_count = 0
+        laps = []
+        print("Stopwatch reset.")
+
+    elif command == "quit":
+        print("Goodbye!")
         break
 
     else:
