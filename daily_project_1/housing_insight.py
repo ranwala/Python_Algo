ac2e5d5aae37bd22bf394982aa9ea82280a560a8
chanduranwala <chanduranwala@gmail.com>
2025-09-18 22:33:12 +0200

    Modify housing insight algo, 18,09

--- daily_project_1/housing_insight.py
+++ daily_project_1/housing_insight.py
@@ -4,11 +4,60 @@ FILE_PATH = "files/california_housing_test.csv"
 total_median_income = 0
 total_median_house_value = 0
 total_population = 0
+group = {}
 
 df = pd.read_csv(FILE_PATH)
 
 housing_dict = df.to_dict(orient='records')
 NUMBER_OF_ROWS = len(housing_dict)
+
+def group_by_median_age():
+    for row in housing_dict:
+        median_age = row['housing_median_age']
+        if median_age not in group:
+            group[median_age] = []
+        group[median_age].append(row)
+
+    for median_age, group_items in group.items():
+        print(f'Housing Median Age: {median_age}')
+
+        avg_house_value = get_avg_house_value_per_age(group_items)
+        print(f'Average House Value per Age: {avg_house_value}')
+        print()
+
+
+def get_avg_house_value_per_age(grouped_item_dict):
+    total = 0
+    for row_item in grouped_item_dict:
+        total += row_item['median_house_value']
+
+    avg = total / len(grouped_item_dict)
+    return round(avg, 2)
+
+
+def filter_by_income():
+    income_group = {}
+    income_input = float(input("Please enter your income: "))
+
+    print(f'Minimum Income: {income_input}')
+
+    for row in housing_dict:
+        house_value = row['median_house_value']
+        if house_value <= income_input:
+            if house_value not in income_group:
+                income_group[house_value] = []
+            income_group[house_value].append(row)
+
+    print('Houses in your income grange')
+    for house_value, group in income_group.items():
+        print(f'House Value: {house_value}')
+
+        for row in group:
+            print(f'Total Rooms: {row["total_rooms"]}', end=' ')
+            print(f'Total Bedrooms: {row["total_bedrooms"]}')
+
+        print()
+
 
 for row in housing_dict:
     total_median_income += row['median_income']
@@ -36,4 +85,9 @@ Highest Income: {max_value} (Row {max_index})
 Total population (income > 6): {total_population}
 """
 
-print(content.strip())
+print(content)
+
+# Group by
+# group_by_median_age()
+
+filter_by_income()
